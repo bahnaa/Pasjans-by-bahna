44ccc816d85c6306114ac4ca6b2f32aded515653
bahnaa <devcristopher96@gmail.com>
2021-08-27 00:10:06 +0200

    Moving cards depends also on the number

--- scripts/game.js
+++ scripts/game.js
@@ -111,7 +111,7 @@ function dragStart() {
     slotTwo.classList.add("active");
     slotThree.classList.add("active");
     slotFour.classList.add("active");
-    this.removeEventListener("click", dragStart)
+    // this.removeEventListener("click", dragStart)
     dragMiddle(this);
 }
 
@@ -141,15 +141,24 @@ function dragEnd() {
 
     if(this.lastElementChild == undefined) {if(!oldThis.id.includes("13")){
         console.log("nie masz asa");
+        cleanActive();
         return;
     } else {
         console.log("masz asa");
         this.appendChild(oldThis);
+        oldThis.removeEventListener("click", dragStart);
     }} else if(this.lastElementChild.id.substring(this.lastElementChild.id.split("").findIndex((sign) => {
     return sign ===" ";})+1, 100)===oldThis.id.substring(oldThis.id.split("").findIndex((sign) => {
     return sign ===" ";})+1, 100)){
+        if(this.lastElementChild.id.substring(0,this.lastElementChild.id.split("").findIndex((sign) => {
+    return sign ===" ";}))===oldThis.lastElementChild.id.substring(0,oldThis.lastElementChild.id.split("").findIndex((sign) => {
+    return sign ===" ";}))+1){
         this.appendChild(oldThis);
-    } else {return};
+        oldThis.removeEventListener("click", dragStart);
+    }
+    };
+
+    console.log("chej");
 
     // if(this.lastElementChild!= undefined) {if(this.lastElementChild.id.includes("diamonds")) {console.log(`Slot ${this.className} just got new diamond card`)}};
 
